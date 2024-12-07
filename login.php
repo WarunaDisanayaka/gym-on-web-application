cc96e23e645a55a02ec529aaf7a7ef4e18c89633
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2024-12-07 22:59:45 +0530

    Registration user function added

--- login.php
+++ login.php
@@ -1,13 +1,12 @@
 <?php
 
-
-include '../../config/db.php'; // Include your db connection script
+require './config/db.php'; // Include your db connection script
 // Initialize variables and error messages
 $name = $email = $password = "";
 $nameErr = $emailErr = $passwordErr = "";
 $successMessage = "";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["register"])) {
 
   // Collect input data and sanitize
   $name = trim($_POST['name']);
@@ -27,6 +26,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $emailErr = "Email is required.";
   } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     $emailErr = "Invalid email format.";
+  } else {
+    // Check if email already exists
+    $emailCheckQuery = "SELECT * FROM users WHERE email = '$email'";
+    $result = mysqli_query($conn, $emailCheckQuery);
+    if (mysqli_num_rows($result) > 0) {
+      $emailErr = "This email is already registered.";
+    }
   }
 
   // Validate Password
@@ -87,6 +93,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <link rel="stylesheet" type="text/css" href="assets/css/style-dark.css"> 
   <!-- Responsive -->
   <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
   <style>
         .error { color: red; font-size: 0.9em; }
@@ -191,9 +198,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <input type="password" name="password" placeholder="Password" value="<?php echo htmlspecialchars($password); ?>">
     <span class="error"><?php echo $passwordErr; ?></span>
+
+    <?php if (!empty($successMessage)) { ?>
+                         <p class="success" style="color:green;"><?php echo $successMessage; ?></p>
+               <?php } ?>
     <br>
               <p>Your personal data will be used to support your experience throughout this website, to manage access to your account, and for other purposes described in our privacy policy.</p>
-              <button type="submit" class="theme-btn"> Register</button>
+              <button type="submit" name="register" class="theme-btn"> Register</button>
             </form>
           </div>
         </div>
@@ -201,221 +212,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </div>
   </section>
   <!-- Login Register End -->
+  <?php include './footer/footer.php' ?>
 
-  <!-- Footer Style One Start -->
-  <footer class="footer-style-one" style="background-image: url(assets/images/footer-img.jpg);">
-    <div class="footer-p-1">
-      <div class="container">
-        <div class="row">
-          <div class="footer-first">
-            <div class="footer-logo">
-              <a href="index.html">
-                <img src="assets/images/logo-2.svg" alt="Footer Logo">
-              </a>
-            </div>
-            <div class="contact-info d-flex-all">
-              <div class="images d-flex-all justify-content-start">
-                <figure>
-                  <img src="assets/images/blog-author-img.jpg" alt="Contact Images">
-                </figure>
-                <figure>
-                  <img src="assets/images/blog-author-img-2.jpg" alt="Contact Images">
-                </figure>
-                <figure>
-                  <img src="assets/images/blog-author-img-3.jpg" alt="Contact Images">
-                </figure>
-              </div>
-              <p>expert trainers <span>+1 (251) 344 0 66</span> free call !</p>
-            </div>
-            <a href="contact.html" class="theme-btn">Get a Consultation </a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="footer-p-2">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-4 col-md-6 col-sm-12">
-            <div class="footer-col">
-              <h3>Information</h3>
-              <p>Regular trips to the gym are great, but don't worry if you can't find a large chunk of time to exercise every day.</p>
-              <ul class="social-media">
-                <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
-                <li><a href="#"><i class="flaticon-twitter"></i></a></li>
-                <li><a href="#"><i class="flaticon-instagram"></i></a></li>
-                <li><a href="#"><i class="flaticon-pinterest"></i></a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-6 col-sm-12">
-            <div class="footer-col">
-              <h3>Contact</h3>
-              <ul>
-                <li>
-                   <i class="flaticon-maps"></i>
-                  <p>901 N Pitt Str., Suite 170, VA 22314, USA</p>
-                </li>
-                <li>
-                   <i class="flaticon-iphone"></i>
-                  <p><a href="callto:(+380)503184707">(+380) 50 318 47 07</a></p>
-                </li>
-                <li>
-                   <i class="flaticon-mail"></i>
-                  <p><a href="mailto:username@domain.com">username@domain.com</a></p>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-6 col-sm-12">
-            <div class="footer-col">
-              <h3>Newsletter</h3>
-              <p>Signup for our weekly newsletter to get the latest news.</p>
-              <form>
-                <input type="email" name="email" placeholder="Enter your email.">
-                <button>
-                  <i class="fa-solid fa-arrow-up-long"></i>
-                </button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <ul class="image-gallery">
-      <li>
-          <a href="assets/images/o-p-o-1.jpg" data-fancybox="gallery">
-              <i class="flaticon-instagram"></i>
-          </a>
-          <figure><img alt="girl" src="assets/images/o-p-o-1.jpg"></figure>
-      </li>
-      <li>
-          <a href="assets/images/o-p-o-2.jpg" data-fancybox="gallery">
-              <i class="flaticon-instagram"></i>
-          </a>
-          <figure><img alt="girl" src="assets/images/o-p-o-2.jpg"></figure>
-      </li>
-      <li>
-          <a href="assets/images/o-p-o-3.jpg" data-fancybox="gallery">
-              <i class="flaticon-instagram"></i>
-          </a>
-          <figure><img alt="girl" src="assets/images/o-p-o-3.jpg"></figure>
-      </li>
-      <li>
-          <a href="assets/images/blog-img-2.jpg" data-fancybox="gallery">
-              <i class="flaticon-instagram"></i>
-          </a>
-          <figure><img alt="girl" src="assets/images/blog-img-2.jpg"></figure>
-      </li>
-      <li>
-          <a href="assets/images/project-img-1.jpg" data-fancybox="gallery">
-              <i class="flaticon-instagram"></i>
-          </a>
-          <figure><img alt="girl" src="assets/images/project-img-1.jpg"></figure>
-      </li>
-      <li>
-          <a href="assets/images/project-img-5.jpg" data-fancybox="gallery">
-              <i class="flaticon-instagram"></i>
-          </a>
-          <figure><img alt="girl" src="assets/images/project-img-5.jpg"></figure>
-      </li>
-      <li>
-          <a href="assets/images/project-img-4.jpg" data-fancybox="gallery">
-              <i class="flaticon-instagram"></i>
-          </a>
-          <figure><img alt="girl" src="assets/images/project-img-4.jpg"></figure>
-      </li> 
-    </ul>
-    <div class="footer-p-3 rights">
-      <div class="container">
-        <div class="row">
-          <div class="footer-col">
-            <p>gym on fitness Center<i class="fa-solid fa-heart"></i> © 2024 <a href="https://winsfolio.net/"> winsfolio.net</a> All rights reserved</p> 
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
-
-  <div class="modal fade popups conslt-popup" id="exampleModal" tabindex="-1" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <img src="assets/images/crain.jpg" alt="img">
-            <div class="contact-form-one popup">
-              <div class="c-form-2">
-                <h3>Start Consulting</h3>
-                <div class="parallax" style="background-image: url(assets/images/pattren.html);"></div>
-                <form>
-                  <div class="row g-0">
-                    <input type="text" class="form-control" id="exampleInputText1"  placeholder="Complete Name">
-                  </div>
-                  <div class="row g-0">
-                    <input type="email" class="form-control" id="exampleInputEmail1"  placeholder="Email Address">
-                  </div>
-                  <div class="row g-0">
-                    <input type="number" class="form-control" id="exampleInputPassword1" placeholder="Phone No">
-                  </div>
-                  <div class="row g-0">
-                    <select id="inputState-21" class="form-control">
-                      <option selected>Subject</option>
-                      <option>Subject 1</option>
-                      <option>Subject 2</option>
-                      <option>Subject 3</option>
-                    </select>
-                  </div>
-                  <div class="row g-0">
-                    <textarea placeholder="Question / Message?"></textarea>
-                  </div>
-                  <button type="submit" class="theme-btn">Submit Now</button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  <!-- Footer Style One End -->
-    
-    <div id="scroll-percentage"><span id="scroll-percentage-value"></span></div>
-
-  <!-- Jquery -->
-
-  <script src="assets/js/jquery.min.js"></script>
-
-  <!-- Waypoint -->
-
-  <script src="assets/js/jquery.waypoints.min.js"></script>
-
-
-  <!-- Counter -->
-
-  <script src="assets/js/jquery.counterup.min.js"></script>
-
-  <!-- Bootstrap Js -->
-
-  <script src="assets/js/bootstrap.min.js"></script>
-
-  <!-- Fancybox Js -->
-
-  <script src="assets/js/jquery.fancybox.min.js"></script> 
-
-  <!-- Nice Select Js -->
-
-  <script src="assets/js/jquery.nice-select.js"></script>
-
-
-  <!-- Owl Carousal Js -->
-
-  <script src="assets/js/owl.carousel.min.js"></script>
-
-  <!-- Custom Js -->
-
-  <script src="assets/js/custom.js"></script>
 </body>
-
-
 <!-- Mirrored from winsfolio.net/html/gymon/gym-on-drak/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 19 Nov 2024 11:19:41 GMT -->
 </html>
